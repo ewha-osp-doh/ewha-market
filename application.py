2bbd229385ac075d90e10061573e5ca4812b6bac
sohyundoh <lyny.doh@gmail.com>
2023-10-26 04:35:26 +0000

    [feat] application.py 초기 세팅

--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 from flask import Flask
-import sys
-application = Flask(__name__)
 
+application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello goorm!"
 
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=int(sys.argv[1]))
+if __name__ == "__main__": 
+    application.run(host='0.0.0.0', debug=True)
