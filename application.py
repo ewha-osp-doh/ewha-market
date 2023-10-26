386fcd2b7d6ce6d3e345637633b264de37580e44
Sohyun Doh <lyny.doh@gmail.com>
2023-10-26 06:02:13 +0000

    [feat] 한줄기 빛이 보이기 시작한다.

--- application.py
+++ application.py
@@ -8,5 +8,18 @@ def hello():
     return render_template("index.html")
 
 
+@application.route("/submit_item")
+def reg_item_submit():
+    name=request.args.get("name")
+    seller=request.args.get("seller") 
+    addr=request.args.get("addr") 
+    email=request.args.get("email") 
+    category=request.args.get("category") 
+    card=request.args.get("card") 
+    status=request.args.get("status") 
+    phone=request.args.get("phone")
+    print(name,addr,tel,category,park,time,site) 
+    return render_template("reg_item.html")
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0', debug=False)
