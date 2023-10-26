4e71d38d3062818e7022a71e7150e010d3bee56e
Sohyun Doh <lyny.doh@gmail.com>
2023-10-26 04:55:04 +0000

    [feat] 과제 1 step

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
-    return "Hello goorm!"
+    return render_template("index.html")
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
