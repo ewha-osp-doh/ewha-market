f0dea874b35592c8f473f386d8bb89c7d3edc215
YeonjuSeo <tjduswn1219@gmail.com>
2023-11-05 02:17:28 +0900

    [style] make invisible file choice btn

--- static/js/reg_reviews.js
+++ static/js/reg_reviews.js
@@ -1,3 +1,31 @@
+// 리뷰 이미지 업로드
+function uploadImage() {
+  document.getElementById("productImage").click();
+}
+
+function checkFile() {
+  var fileInput = document.getElementById("productImage");
+  var file = fileInput.files[0];
+
+  if (file) {
+    var fileType = file.type;
+    var fileSize = file.size;
+
+    if (fileType !== "image/jpeg" && fileType !== "image/png") {
+      alert("이미지 파일은 jpg 또는 png 형식이어야 합니다.");
+      fileInput.value = "";
+    } else if (fileSize > 5000000) {
+      // 5MB
+      alert("이미지 파일의 크기는 5MB 이하여야 합니다.");
+      fileInput.value = "";
+    } else {
+      var imgPreview = document.getElementById("imagePreview");
+      imgPreview.src = URL.createObjectURL(file);
+    }
+  }
+}
+
+// 별점
 const stars = document.querySelectorAll('.rate input[type="radio"]');
 const ratingText = document.getElementById("ratingText");
 
