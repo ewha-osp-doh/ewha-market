4007b7b974051ac27ea2290280408dca2807792d
yunyun827 <yunyun827@naver.com>
2023-11-21 08:12:17 +0000

    application.py 오타 수정, Login_lab.html 수정

--- application.py
+++ application.py
@@ -57,9 +57,9 @@ def login_user():
     id = request.form['id']
     pw = request.form['pw']
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    if DB.find_user(id_,pw_hash):
-        session['id']=id_
-        return redirect(url_for)
+    if DB.find_user(id, pw_hash):
+        session['id'] = id
+        return redirect(url_for('view_list'))
     else:
         flash("Wrong ID or PW!")
         return render_template("login.html")
