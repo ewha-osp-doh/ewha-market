13e867aed72e983a7be362a30ecd5a99f84fdcba
김예지 <yeji.lee.kim@gmail.com>
2023-11-30 08:13:39 +0000

    application.py 메서드 순서 변경

--- application.py
+++ application.py
@@ -16,6 +16,82 @@ DB = DBhandler()
 def hello():
     return render_template("index.html")
 
+# 회원가입
+@application.route("/signup")
+def signup():
+    return render_template("Signup.html")
+
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw = request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data, pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("Signup.html")
+
+# 로그인
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+@application.route("/login_confirm", methods=['POST']) 
+def login_user():
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest() 
+    if DB.find_user(id_ ,pw_hash):
+        session['id'] = id_
+        return redirect(url_for('view_list'))
+    else:
+        flash("Wrong ID or PW!")
+        return render_template("login.html")
+
+
+# 로그아웃
+@application.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for('view_list'))
+
+
+# 세션 체크
+@application.route("/check-session")
+def check_session():
+    user_id = session.get('id')
+    return jsonify(isLoggedIn=bool(user_id), userId=user_id)
+
+
+# 상품 등록
+@application.route("/reg_items", methods=['GET', 'POST']) 
+def reg_item():
+    return render_template("reg_items.html")
+
+
+@application.route("/submit_items_post", methods=['POST']) 
+def reg_item_submit_post():
+    file = request.files['productImage']
+    data = {
+        "seller-id" : request.form.get("sellerId"),
+        "product-name" : request.form.get("productName"),
+        "product-price" : request.form.get("productPrice"),
+        "product-status" : request.form.get("condition"),
+        "product-description" : request.form.get("productDescription")
+    } 
+    if 'productImage' in request.files:
+        file = request.files['productImage']
+        # 파일을 어디에 저장할지 결정하고 저장합니다.
+        img_path = "./static/images/" + file.filename
+        file.save(img_path)
+        data['img_path'] = '../static/images/' +  file.filename
+    
+    DB.insert_item(data['product-name'], data)
+    return render_template("submit_item_result.html", data=data)
+
+
+# 상품 전체 조회
 @application.route("/list")
 def view_list():
     items = DB.get_all_items()
@@ -23,6 +99,64 @@ def view_list():
     total_pages = (len(items) + items_per_page - 1) // items_per_page  # 페이지 수 계산
     return render_template('item.html', items=items, totalPages=total_pages, currentPage = 1, itemsPerPage = 6)
 
+
+# 상품 상세 조회
+@application.route("/view_detail/<name>/") 
+def view_item_detail(name):
+    print("###name:",name)
+    data = DB.get_item_byname(str(name)) 
+    print("####data:",data)
+    return render_template("items_detailed.html", name=name, data=data)
+
+
+# 좋아요 조회
+
+@application.route('/show_heart/<name>/', methods=['GET'])
+def show_heart(name):
+    my_heart = DB.get_heart_byname(session['id'],name)
+    return jsonify({'my_heart': my_heart})
+
+
+# 좋아요 등록
+@application.route('/like/<name>/', methods=['POST'])
+def like(name):
+    my_heart = DB.update_heart(session['id'],'Y',name)
+    return jsonify({'msg': '좋아요 완료!'})
+
+
+# 안좋아요 등록
+@application.route('/unlike/<name>/', methods=['POST'])
+def unlike(name):
+    my_heart = DB.update_heart(session['id'],'N',name)
+    return jsonify({'msg': '안좋아요 완료!'})
+
+
+# 리뷰 등록
+@application.route("/reg_review_init/<name>/") 
+def reg_review_init(name):
+    return render_template("reg_reviews.html", name=name)
+
+@application.route("/reg_review", methods=['POST']) 
+def reg_review():
+    data = {
+        "title": request.form.get('reviewTitle'),
+        "point": request.form.get('rating'),
+        "content": request.form.get('reviewText'),
+        "authorId": request.form.get('reviewerId'),
+        "productName": request.form.get('productName')
+    }
+    if 'productImage' in request.files:
+        file = request.files['productImage']
+        # 파일을 어디에 저장할지 결정하고 저장합니다.
+        img_path = "./static/images/" + file.filename
+        file.save(img_path)
+        data['img_path'] = '../static/images/' + file.filename
+    
+    DB.reg_review(data)
+    return redirect(url_for('view_review'))
+
+
+# 리뷰 전체 조회
 @application.route("/review")
 def view_review():
     page = request.args.get("page", 0, type=int)
@@ -50,129 +184,14 @@ def view_review():
         page_count=int((item_counts/per_page)+1),
         total=item_counts)
 
-@application.route("/reg_review_init/<name>/") 
-def reg_review_init(name):
-    return render_template("reg_reviews.html", name=name)
 
-@application.route("/reg_review", methods=['POST']) 
-def reg_review():
-    data = {
-        "title": request.form.get('reviewTitle'),
-        "point": request.form.get('rating'),
-        "content": request.form.get('reviewText'),
-        "authorId": request.form.get('reviewerId'),
-        "productName": request.form.get('productName')
-    }
-    if 'productImage' in request.files:
-        file = request.files['productImage']
-        # 파일을 어디에 저장할지 결정하고 저장합니다.
-        img_path = "./static/images/" + file.filename
-        file.save(img_path)
-        data['img_path'] = '../static/images/' + file.filename
-    
-    DB.reg_review(data)
-    return redirect(url_for('view_review'))
-
-@application.route("/reg_items", methods=['GET', 'POST']) 
-def reg_item():
-    return render_template("reg_items.html")
-
-
-@application.route("/submit_items_post", methods=['POST']) 
-def reg_item_submit_post():
-    file = request.files['productImage']
-    data = {
-        "seller-id" : request.form.get("sellerId"),
-        "product-name" : request.form.get("productName"),
-        "product-price" : request.form.get("productPrice"),
-        "product-status" : request.form.get("condition"),
-        "product-description" : request.form.get("productDescription")
-    } 
-    if 'productImage' in request.files:
-        file = request.files['productImage']
-        # 파일을 어디에 저장할지 결정하고 저장합니다.
-        img_path = "./static/images/" + file.filename
-        file.save(img_path)
-        data['img_path'] = '../static/images/' +  file.filename
-    
-    DB.insert_item(data['product-name'], data)
-    return render_template("submit_item_result.html", data=data)
-
-
-@application.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@application.route("/login_confirm", methods=['POST']) 
-def login_user():
-    id_=request.form['id']
-    pw=request.form['pw']
-    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest() 
-    if DB.find_user(id_ ,pw_hash):
-        session['id'] = id_
-        return redirect(url_for('view_list'))
-    else:
-        flash("Wrong ID or PW!")
-        return render_template("login.html")
-
-@application.route("/logout")
-def logout_user():
-    session.clear()
-    return redirect(url_for('view_list'))
-
-@application.route("/signup")
-def signup():
-    return render_template("Signup.html")
-
-
-@application.route("/signup_post", methods=['POST'])
-def register_user():
-    data=request.form
-    pw = request.form['pw']
-    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
-    if DB.insert_user(data, pw_hash):
-        return render_template("login.html")
-    else:
-        flash("user id already exist!")
-        return render_template("Signup.html")
-
-@application.route("/check-session")
-def check_session():
-    user_id = session.get('id')
-    return jsonify(isLoggedIn=bool(user_id), userId=user_id)
-
-@application.route("/view_detail/<name>/") 
-def view_item_detail(name):
-    print("###name:",name)
-    data = DB.get_item_byname(str(name)) 
-    print("####data:",data)
-    return render_template("items_detailed.html", name=name, data=data)
-
-
+# 리뷰 상세 조회
 @application.route("/review_detail/<name>/")
 def view_review_detail(name):
     review_data = DB.get_review_byname(name)
     print(review_data)
     return render_template("review_detailed.html", review=review_data)
 
-
-@application.route('/show_heart/<name>/', methods=['GET'])
-def show_heart(name):
-    my_heart = DB.get_heart_byname(session['id'],name)
-    return jsonify({'my_heart': my_heart})
-
-
-@application.route('/like/<name>/', methods=['POST'])
-def like(name):
-    my_heart = DB.update_heart(session['id'],'Y',name)
-    return jsonify({'msg': '좋아요 완료!'})
-
-
-@application.route('/unlike/<name>/', methods=['POST'])
-def unlike(name):
-    my_heart = DB.update_heart(session['id'],'N',name)
-    return jsonify({'msg': '안좋아요 완료!'})
 
 
 if __name__ == "__main__":
