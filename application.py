9b42eb7e212353e3ed3a7517b6ca0703521c64b0
­서연주(엘텍공과대학 소프트웨어학부) <kados22@ewhain.net>
2023-12-04 02:32:49 +0000

    [fix] update codes for operating functions

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from database import DBhandler
 
+import hashlib
 import sys
 
 
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "hello_osp"
 
 
 DB = DBhandler()
@@ -14,39 +16,183 @@ DB = DBhandler()
 def hello():
     return render_template("index.html")
 
-@application.route("/list")
-def view_list():
-    return render_template("item.html")
+# 회원가입
+@application.route("/signup")
+def signup():
+    return render_template("Signup.html")
+
+@application.route("/signup_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw = request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data, pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("Signup.html")
+
+# 로그인
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+@application.route("/login_confirm", methods=['POST']) 
+def login_user():
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest() 
+    if DB.find_user(id_ ,pw_hash):
+        session['id'] = id_
+        return redirect(url_for('view_list'))
+    else:
+        flash("Wrong ID or PW!")
+        return render_template("login.html")
 
 
-@application.route("/review") 
-def view_review():
-    return render_template("review.html")
+# 로그아웃
+@application.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for('view_list'))
 
 
+# 세션 체크
+@application.route("/check-session")
+def check_session():
+    user_id = session.get('id')
+    return jsonify(isLoggedIn=bool(user_id), userId=user_id)
+
+
+# 상품 등록
 @application.route("/reg_items", methods=['GET', 'POST']) 
 def reg_item():
     return render_template("reg_items.html")
 
 
-@application.route("/reg_reviews") 
-def reg_review():
-    return render_template("reg_reviews.html")
-
-
 @application.route("/submit_items_post", methods=['POST']) 
 def reg_item_submit_post():
+    file = request.files['productImage']
     data = {
         "seller-id" : request.form.get("sellerId"),
         "product-name" : request.form.get("productName"),
         "product-price" : request.form.get("productPrice"),
         "product-status" : request.form.get("condition"),
         "product-description" : request.form.get("productDescription")
-    }
+    } 
+    if 'productImage' in request.files:
+        file = request.files['productImage']
+        # 파일을 어디에 저장할지 결정하고 저장합니다.
+        img_path = "./static/images/" + file.filename
+        file.save(img_path)
+        data['img_path'] = '../static/images/' +  file.filename
+    
     DB.insert_item(data['product-name'], data)
     return render_template("submit_item_result.html", data=data)
 
 
+# 상품 전체 조회
+@application.route("/list")
+def view_list():
+    items = DB.get_all_items()
+    items_per_page = 6
+    total_pages = (len(items) + items_per_page - 1) // items_per_page  # 페이지 수 계산
+    return render_template('item.html', items=items, totalPages=total_pages, currentPage = 1, itemsPerPage = 6)
+
+
+# 상품 상세 조회
+@application.route("/view_detail/<name>/") 
+def view_item_detail(name):
+    print("###name:",name)
+    data = DB.get_item_byname(str(name)) 
+    print("####data:",data)
+    return render_template("items_detailed.html", name=name, data=data)
+
+
+# 좋아요 조회
+
+@application.route('/show_heart/<name>/', methods=['GET'])
+def show_heart(name):
+    my_heart = DB.get_heart_byname(session['id'],name)
+    return jsonify({'my_heart': my_heart})
+
+
+# 좋아요 등록
+@application.route('/like/<name>/', methods=['POST'])
+def like(name):
+    my_heart = DB.update_heart(session['id'],'Y',name)
+    return jsonify({'msg': '좋아요 완료!'})
+
+
+# 안좋아요 등록
+@application.route('/unlike/<name>/', methods=['POST'])
+def unlike(name):
+    my_heart = DB.update_heart(session['id'],'N',name)
+    return jsonify({'msg': '안좋아요 완료!'})
+
+
+# 리뷰 등록
+@application.route("/reg_review_init/<name>/") 
+def reg_review_init(name):
+    return render_template("reg_reviews.html", name=name)
+
+@application.route("/reg_review", methods=['POST']) 
+def reg_review():
+    data = {
+        "title": request.form.get('reviewTitle'),
+        "point": request.form.get('rating'),
+        "content": request.form.get('reviewText'),
+        "authorId": request.form.get('reviewerId'),
+        "productName": request.form.get('productName')
+    }
+    if 'productImage' in request.files:
+        file = request.files['productImage']
+        # 파일을 어디에 저장할지 결정하고 저장합니다.
+        img_path = "./static/images/" + file.filename
+        file.save(img_path)
+        data['img_path'] = '../static/images/' + file.filename
+    
+    DB.reg_review(data)
+    return redirect(url_for('view_review'))
+
+
+# 리뷰 전체 조회
+@application.route("/review")
+def view_review():
+    page = request.args.get("page", 0, type=int)
+    per_page=6 # item count to display per page
+    per_row=3# item count to display per row
+    row_count=int(per_page/per_row)
+    start_idx=per_page*page
+    end_idx=per_page*(page+1)
+    data = DB.get_all_reviews() #read the table
+    item_counts = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    for i in range(row_count):#last row
+        if (i == row_count-1) and (tot_count%per_row != 0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:])
+        else: 
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:(i+1)*per_row])
+    return render_template(
+        "review_overview.html",
+        datas=data.items(),
+        row1=locals()['data_0'].items(),
+        row2=locals()['data_1'].items(),
+        limit=per_page,
+        page=page,
+        page_count=int((item_counts/per_page)+1),
+        total=item_counts)
+
+
+# 리뷰 상세 조회
+@application.route("/review_detail/<name>/")
+def view_review_detail(name):
+    review_data = DB.get_review_byname(name)
+    print(review_data)
+    return render_template("review_detailed.html", review=review_data)
+
+
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=False)
-    
--- database.py
+++ database.py
@@ -6,20 +6,132 @@ class DBhandler:
     def __init__(self):
         with open('./authentication/firebase_auth.json') as f:
             config = json.load(f)
+
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
-       
+
+    # User
+    
+    def insert_user(self, data, pw):
+        user_info = {
+            "id": data['id'],
+            "password": pw,
+            "nickname": data['nickname']
+        }
+        if self.user_duplicate_check(str(data['id'])):
+            self.db.child("user").push(user_info)
+            print(data)
+            return True
+        else:
+            return False
+    
+    def user_duplicate_check(self, id_string):
+        users = self.db.child("user").get()
+
+        print("users###", users.val())
+        if str(users.val()) == "None": # first registration
+            return True
+        else:
+            for res in users.each():
+                value = res.val()
+
+                if value['id'] == id_string:
+                    return False
+            return True
+        
+    def find_user(self, id_, pw_):
+        users = self.db.child("user").get() 
+        target_value=[]
+        for res in users.each():
+            value = res.val()
+            
+            if value['id'] == id_ and value['password'] == pw_:
+                return True 
+            
+        return False
+        
+        
+    # Item
+    
     def insert_item(self, name, data):
         item_info ={
             "sellerId": data['seller-id'],
             "productName": data['product-name'],
             "productPrice": data['product-price'],
-            # "currentLocation": data['currentLocation'],
             "product-status": data['product-status'],
             "description": data['product-description'],
-            # "product-image": img_path
+            "product-image": data['img_path']
         }
         self.db.child("item").child(name).set(item_info)
         print(data)
         return True
-        
+       
+    def get_all_items(self):
+        items = self.db.child("item").get()
+        result = []
+
+        if items.val():
+            for item in items.each():
+                result.append(item.val())
+
+        return result
+    
+    def get_item_byname(self, name): 
+        items = self.db.child("item").get() 
+        target_value="" 
+        print("###########",name)
+        for res in items.each(): 
+            key_value = res.key()
+            if key_value == name: 
+                target_value=res.val()
+        return target_value
+    
+    def get_heart_byname(self, uid, name):
+        hearts = self.db.child("heart").child(uid).get()
+        target_value=""
+        if hearts.val() == None:
+            return target_value
+
+        for res in hearts.each():
+            key_value = res.key()
+
+        if key_value == name:
+            target_value=res.val()
+        return target_value
+    
+    def update_heart(self, user_id, isHeart, item):
+        heart_info ={
+             "interested": isHeart
+        }
+        self.db.child("heart").child(user_id).child(item).set(heart_info)
+        return True
+
+    
+    # Review
+    
+    def reg_review(self, data):
+        review_info = {
+            "title": data['title'],
+            "point": data['point'],
+            "content": data['content'],
+            "img_path": data['img_path'],
+            "authorId": data['authorId']
+        }
+        self.db.child("review").child(data['productName']).set(review_info)
+        return True
+      
+    def get_all_reviews(self ):
+        reviews = self.db.child("review").get().val()
+        return reviews
+    
+    def get_review_byname(self, name): 
+        reviews = self.db.child("review").get() 
+        target_value="" 
+        print("###########",name)
+        for res in reviews.each():
+            key_value = res.key()
+            if key_value == name: 
+                target_value=res.val()
+                break
+
+        return target_value
