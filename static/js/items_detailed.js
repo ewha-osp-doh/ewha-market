35b4763e44143639cc96a030c8a122b85c499bed
yunyun827 <yunyun827@naver.com>
2023-12-06 18:04:05 +0000

    상태 표시 성공!

--- static/js/items_detailed.js
+++ static/js/items_detailed.js
@@ -32,17 +32,21 @@
 
 
 function statusSetting(){
-    var status = documnet.getElementByID("statusSet")
-    var statusView = documnet.getElementByID("statusView")
+    var status = documnet.getElementById("statusSet");
+    var statusView = documnet.getElementById("statusView");
+    console.log(status.innerText);
     
     if (status === '상') {
          // 아이콘을 활성화 상태로 변경
-        statusView.src = "/static/assets/great_active.svg"
+            statusView.src = "../static/assets/great_active.svg";
+            
       } else if (status === '중'){
        // 아이콘을 비활성화 상태로 변경
-       setStatusIcon('/static/assets/good_active.svg'); 
+           statusView.src = "../static/assets/good_active.svg";
       } else if (status === '하'){
-                    // 아이콘을 비활성화 상태로 변경
-                    setStatusIcon('/static/assets/soso_active.svg'); 
-                }
+      // 아이콘을 비활성화 상태로 변경
+            statusView.src = "../static/assets/soso_active.svg";
+      }
 }
+
+statusSetting();
